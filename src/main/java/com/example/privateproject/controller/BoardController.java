bbc6687315e4f8e2c3d1c5b39e8799854d2ee1af
Ungjo <ungjo911@gmail.com>
2026-06-16 03:59:20 +0900

    2026.06.16 집에서 수정. 서버작동 안함

--- src/main/java/com/example/privateproject/controller/BoardController.java
+++ src/main/java/com/example/privateproject/controller/BoardController.java
@@ -1,6 +1,24 @@
 package com.example.privateproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.privateproject.service.BoardService;
+
+@Controller
+@RequestMapping("/board")
 public class BoardController {
 	
+	@Autowired
+	private BoardService boardService;
 	
+//	------------------
+	
+//	[게시글 작성 처리] POST /board/write
+	@PostMapping("/write")
+	public String Write(BoardDTO boardDTO, )
 }
--- src/main/java/com/example/privateproject/service/BoardService.java
+++ src/main/java/com/example/privateproject/service/BoardService.java
@@ -1,5 +1,26 @@
 package com.example.privateproject.service;
 
-public class BoardService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.privateproject.dto.BoardDTO;
+import com.example.privateproject.mapper.BoardMapper;
+
+@Service
+public class BoardService {
+	
+	@Autowired
+	private BoardMapper boardMapper;
+	
+//	[전체 게시글 수]
+	
+//	[게시글 상세 조회]
+	
+//	[조회수 증가]
+	
+//	[게시글 등록]
+	
+	
 }
